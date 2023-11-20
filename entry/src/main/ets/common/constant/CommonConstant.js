6f7c8ab20f3d42021fe54a2e8a103a6c35db4930
binbin1760 <569279172@qq.com>
2023-11-21 03:55:44 +0800

    complate demo

---
+++ entry/src/main/ets/common/constant/CommonConstant.js
@@ -0,0 +1,19 @@
+export default class CommonConstants {
+}
+// 常用字段
+CommonConstants.FULL_LENGTH = '100%';
+CommonConstants.TITLE_WIDTH = '80%';
+CommonConstants.LIST_DEFAULT_WIDTH = '93.3%';
+CommonConstants.OPACITY_DEFAULT = 1;
+CommonConstants.OPACITY_COMPLETED = 0.4;
+CommonConstants.BORDER_RADIUS = 24;
+CommonConstants.FONT_WEIGHT = 500;
+CommonConstants.COLUMN_SPACE = 16;
+CommonConstants.TODO_DATA = [
+    "早起晨练",
+    "准备早餐",
+    "阅读名著",
+    "学习ArkTS",
+    "看剧放松"
+];
+//# sourceMappingURL=CommonConstant.js.map
